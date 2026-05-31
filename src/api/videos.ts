b659785277b0e67f450d8600e504c936cb3049b6
P3truts <obreja.petrut@gmail.com>
2026-05-31 16:00:26 +0300

    E: add security

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -3,13 +3,14 @@ import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, UserForbiddenError } from "./errors";
 import { getBearerToken, validateJWT } from "../auth";
-import { getVideo, updateVideo } from "../db/videos";
+import { getVideo, updateVideo, type Video } from "../db/videos";
 import path from "path";
 import { randomBytes } from "crypto";
 
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
+  console.log("video id before guard: ", videoId);
   if (!videoId) {
     throw new BadRequestError("Invalid video ID");
   }
@@ -20,8 +21,9 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const userID = validateJWT(token, cfg.jwtSecret);
 
   console.log("uploading video", videoId, "by user", userID);
-
   const videoMeta = getVideo(cfg.db, videoId);
+
+  console.log("video.userID:", videoMeta?.userID, "jwt userID:", userID);
   if (videoMeta?.userID !== userID) {
     throw new UserForbiddenError("Forbidden");
   }
@@ -48,26 +50,28 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const videoFilePath = `${cfg.assetsRoot}/${key}`;
   const videoPath = path.join(process.cwd(), videoFilePath);
   await Bun.write(videoPath, arrayBufer);
+  const outputFilePath = await processVideoForFastStart(videoPath);
 
-  console.log("Video path: ", videoPath);
-  const ratio = await getVideoAspectRatio(videoPath);
+  console.log("Video path: ", outputFilePath);
+  const ratio = await getVideoAspectRatio(outputFilePath);
   const prefixedKey = `${ratio}/${key}`;
 
   console.log({ fileName, fileExtension, key: prefixedKey });
-  const videoFile = cfg.s3Client.file(prefixedKey);
-  await videoFile.write(arrayBufer, {
+  const tempFile = Bun.file(outputFilePath);
+  const videoFile = cfg.s3Client.file(prefixedKey, { bucket: cfg.s3Bucket });
+  await videoFile.write(tempFile, {
     type: mediaType,
   });
 
-  const videoURL = `https://tubely-909090.s3.eu-north-1.amazonaws.com/${prefixedKey}`;
-  console.log("VideoURL: ", videoURL);
-  videoMeta.videoURL = videoURL;
+  // const videoURL = `https://tubely-909090.s3.eu-north-1.amazonaws.com/${prefixedKey}`;
+  // console.log("VideoURL: ", videoURL);
+  videoMeta.videoURL = prefixedKey;
 
   updateVideo(cfg.db, videoMeta);
-  const tempFile = Bun.file(videoPath);
   await tempFile.delete();
 
-  return respondWithJSON(200, videoMeta);
+  const presignedVideo = await dbVideoToSignedVideo(cfg, videoMeta);
+  return respondWithJSON(200, presignedVideo);
 }
 
 async function getVideoAspectRatio(filePath: string) {
@@ -77,7 +81,7 @@ async function getVideoAspectRatio(filePath: string) {
 
   const res = await proc.exited;
   if (res != 0) {
-    console.log("There is an error!");
+    console.log("There is an error determining the ratio of the video!");
   }
 
   console.log("Stdout: ", stdoutText);
@@ -95,3 +99,40 @@ async function getVideoAspectRatio(filePath: string) {
     return "other";
   }
 }
+
+async function processVideoForFastStart(inputFilePath: string) {
+  const outputFilePath = inputFilePath + ".processed";
+
+
+  const proc = Bun.spawn(["ffmpeg", "-i", inputFilePath, "-movflags", "faststart", "-map_metadata", "0", "-codec", "copy", "-f", "mp4", outputFilePath], { stdout: "pipe", stderr: "pipe" });
+  const stdoutText = await new Response(proc.stdout).text();
+  const stderrText = await new Response(proc.stderr).text();
+
+  const res = await proc.exited;
+  if (res != 0) {
+    console.log("There is an error processing the video!");
+  }
+
+  console.log("Stdout: ", stdoutText);
+  console.log("Stderr: ", stderrText);
+
+  return outputFilePath;
+}
+
+async function generatePresignedURL(cfg: ApiConfig, key: string, expireTime: number) {
+  const presignedURL = cfg.s3Client.presign(key, { bucket: cfg.s3Bucket, expiresIn: expireTime });
+
+  return presignedURL;
+}
+
+export async function dbVideoToSignedVideo(cfg: ApiConfig, video: Video) {
+  if (!video.videoURL) {
+    return video;
+  } else {
+    const presignedURL = await generatePresignedURL(cfg, video.videoURL, 3600);
+    video.videoURL = presignedURL;
+  }
+
+  return video;
+}
+
